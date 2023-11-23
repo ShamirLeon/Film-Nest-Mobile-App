aff06beeb6f9b998366e981d2a575e51b9935e04
Shamir León <leonshamir1@gmail.com>
2023-11-23 14:36:56 -0500

    Add Movie Provider

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native'
 import Main from './src/components/Main';
+import MovieProvider from './src/context/MovieProvider';
 
 export default function App() {
   return (
     <NativeRouter>
-      <StatusBar style='auto'></StatusBar>
-      <Main />
+      <MovieProvider>
+        <StatusBar style='auto'></StatusBar>
+        <Main />
+      </MovieProvider>
     </NativeRouter>
   );
 }
