3c5b73fba78b64a1fdf559d4c4f3d4149d97760f
Shamir León <leonshamir1@gmail.com>
2023-11-23 16:30:23 -0500

    Add: Get genres and genresMap

--- src/context/MovieProvider.tsx
+++ src/context/MovieProvider.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react'
+import AxiosConfig from "../axios-config";
 import { MovieContext } from "./MovieContext";
+import { IGenre } from '../types/movies.interface';
 
 export default function MovieProvider({ children }: { children: React.ReactNode }) {
 
+    const [genres, setGenres] = useState<IGenre[]>();
+
+    const getGenres = async () => {
+        try {
+            const response = await AxiosConfig('/genre/movie/list?language=en');
+            setGenres(response.data.genres);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const genresMap = new Map<number, string>();
+    genres?.forEach((genre) => {
+        genresMap.set(genre.id, genre.name)
+    })
+    const saludo = 'hola'
+
+    useEffect(() => {
+        getGenres();
+    }, [])
+
+
     return (
-        <MovieContext.Provider value={''}>
+        <MovieContext.Provider value={{
+            genresMap
+        }}>
             {children}
         </MovieContext.Provider>
     )
