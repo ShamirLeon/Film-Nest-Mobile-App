9aac856fcb56f52f590ac4b71da88445843d3d02
Shamir León <leonshamir1@gmail.com>
2023-11-23 16:30:45 -0500

    Add Movie List by Genre page

---
+++ src/app/MoviesByGenre.tsx
@@ -0,0 +1,9 @@
+import StyledText from "../components/StyledText"
+
+function MoviesByGenre() {
+    return (
+        <StyledText>List movies by genre</StyledText>
+    )
+}
+
+export default MoviesByGenre
