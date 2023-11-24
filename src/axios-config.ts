38ba36665174f61f8088ec3a3f5d80facec0cd12
Shamir León <leonshamir1@gmail.com>
2023-11-24 17:04:43 -0500

    Add params

--- src/axios-config.ts
+++ src/axios-config.ts
@@ -3,6 +3,7 @@ import { API_PUBLIC_TOKEN_TMDB } from '@env'
 
 const AxiosConfig = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
+    params: { language: 'en-US' },
     headers: {
         accept: 'application/json',
         Authorization: `Bearer ${API_PUBLIC_TOKEN_TMDB}`
