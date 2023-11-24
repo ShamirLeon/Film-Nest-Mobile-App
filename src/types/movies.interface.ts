caa806225fc10dcc4087cf8849bd5ba16e430ad1
Shamir León <leonshamir1@gmail.com>
2023-11-24 17:05:02 -0500

    add person details interface

--- src/types/movies.interface.ts
+++ src/types/movies.interface.ts
@@ -179,3 +179,22 @@ export interface Genre {
     id: number;
     name: string;
 }
+
+/* Person Interfaces */
+
+export interface IPersonDetails {
+    adult: boolean;
+    also_known_as: string[];
+    biography: string;
+    birthday: Date;
+    deathday: null;
+    gender: number;
+    homepage: null;
+    id: number;
+    imdb_id: string;
+    known_for_department: string;
+    name: string;
+    place_of_birth: string;
+    popularity: number;
+    profile_path: string;
+}
